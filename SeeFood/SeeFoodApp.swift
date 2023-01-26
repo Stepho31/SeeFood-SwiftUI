cdf975e810abdaaeb2bb32f963433e6becaaebd8
Stephen Byron Cameron Burdine <sincceee96@gmail.com>
2023-01-25 21:30:15 -0600

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Stephen Byron on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
